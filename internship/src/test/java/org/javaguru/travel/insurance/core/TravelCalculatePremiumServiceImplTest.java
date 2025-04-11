f55605cdb8a580ca405c6676302e32d481d363cc
Vitali Kalaskou <kolosokvit@gmail.com>
2025-04-11 09:13:36 +0300

    Task_23 done

--- internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ internship/src/test/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -2,41 +2,46 @@ package org.javaguru.travel.insurance.core;
 
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TravelCalculatePremiumServiceImplTest {
+    private DateTimeService dateTimeService;
+    private TravelCalculatePremiumServiceImpl service;
+    private TravelCalculatePremiumRequest request;
 
-    DateTimeService dateTimeService = new DateTimeService();
-    TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl(dateTimeService);
-
+    @BeforeEach
+    void setUp() {
+        request = createRequest();
+        dateTimeService = Mockito.mock(DateTimeService.class);
+        Mockito.when(dateTimeService.calculateDurationInDays(request.getAgreementDateFrom(), request.getAgreementDateTo())).thenReturn(0L);
+        service = new TravelCalculatePremiumServiceImpl(dateTimeService);
+    }
     @Test
     void shouldPopulateResponseWithCorrectPersonFirstName() {
-        TravelCalculatePremiumRequest request = createRequest();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getPersonFirstName(), response.getPersonFirstName());
     }
 
     @Test
     void shouldPopulateResponseWithCorrectPersonLastName() {
-        TravelCalculatePremiumRequest request = createRequest();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getPersonLastName(), response.getPersonLastName());
     }
 
     @Test
     void shouldPopulateResponseWithCorrectDateFrom() {
-        TravelCalculatePremiumRequest request = createRequest();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getAgreementDateFrom(), response.getAgreementDateFrom());
     }
 
     @Test
     void shouldPopulateResponseWithCorrectDateTo() {
-        TravelCalculatePremiumRequest request = createRequest();
         TravelCalculatePremiumResponse response = service.calculatePremium(request);
         assertEquals(request.getAgreementDateTo(), response.getAgreementDateTo());
     }
